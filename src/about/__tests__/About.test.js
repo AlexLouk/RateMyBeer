d8a9662055fa403c9a647c1652647b2acc31ba84
Alexandros Loukaridis <92loal1bif@hft-stuttgart.de>
2023-05-08 22:26:03 +0200

    changed .yml to react version 18. Removed tests that are not yet implemented.

--- src/about/__tests__/About.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from '../About';
-
-//TODO write tests
-
-describe('About', () => {
- expect(true).toBe(true);
-});
--- src/login/__tests__/LoginView.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import LoginView from '../LoginView';
-
-//TODO write tests
-
-describe('LoginView', () => {
-  expect(true).toBe(true);
-});
