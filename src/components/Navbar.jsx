48f8743193250b80a83f20964e95ed170f7b656f
AlexLouk <AlexLouk@icloud.com>
2023-06-01 23:27:16 +0200

    changes on navbar and routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../logos/favicon-32x32.png';
+import './Navbar.css'
+
+
+
+
+
+class Navigation extends Component {
+
+
+    render() {
+        return (
+            <Navbar bg="light" >
+                <Navbar.Brand className='ml-auto' as={Link} to={'/'}>
+                    <img id='navLogo' alt='' src={logo} width='50' height='50' />
+                    RateMyBeer
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <div className='midBlock'>
+                        <Nav className="ms-auto">
+                            <Nav.Link className='nav-link-mid' as={Link} to={'/'}>Home</Nav.Link>
+                            <Nav.Link className='nav-link-mid' as={Link} to={'/rating'}>Rating</Nav.Link>
+                            <Nav.Link className='nav-link-mid' as={Link} to={'/about'} >About</Nav.Link>
+                            <Nav.Link className='nav-link-mid' as={Link} to={'/faqs'}>FAQs</Nav.Link>
+                            <Nav.Link className='nav-link-mid' as={Link} to={'/user'}>User</Nav.Link>
+                        </Nav>
+                    </div>
+                    <div className='rightBlock'>
+                    <Nav className="ms-auto">
+                        <Nav.Link className='nav-link-right' as={Link} to={'/login'}>Login</Nav.Link>
+                        <Nav.Link className='nav-link-right' as={Link} to={'/register'}>Register</Nav.Link>
+                    </Nav>
+                    </div>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default Navigation;
