a1dfaa865ee8224a9ef077675d49fcf3498446a0
AlexLouk <AlexLouk@icloud.com>
2023-06-01 23:26:51 +0200

    changes on navbar and routing put in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
-import NavbarComp from './components/NavbarComp';
-
+import Navbar from './components/Navbar';
+import Home from './home/Home';
+import User from './user/User';
+import Rating from './rating/Rating';
+import About from './about/About';
+import Login from './login/Login';
+import FAQs from './FAQs/FAQs';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
-      <NavbarComp />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/rating" element={<Rating />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/FAQs" element={<FAQs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
