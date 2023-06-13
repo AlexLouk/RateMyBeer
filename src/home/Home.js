ac734284492494e0602755dad6ad31ca2c89e1c2
neobajerke <neo>
2023-06-13 16:43:09 +0200

    Homepage commet feld

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,7 +3,47 @@ import Button from 'react-bootstrap/Button';
 import './Home.css';
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentForms: {}
+    };
+  }
+
+  handleCommentButtonClick = (index) => {
+    this.setState((prevState) => ({
+      commentForms: {
+        ...prevState.commentForms,
+        [index]: true
+      }
+    }));
+  };
+
+  handleCommentChange = (event, index) => {
+    const { value } = event.target;
+    this.setState((prevState) => ({
+      commentForms: {
+        ...prevState.commentForms,
+        [index]: value
+      }
+    }));
+  };
+
+  handleSubmitComment = (event, index) => {
+    event.preventDefault();
+    const comment = this.state.commentForms[index];
+    // Hier kannst du den Kommentar an das Backend senden oder die gewünschte Aktion ausführen
+    console.log('Kommentar:', comment);
+    // Zurücksetzen des Kommentarfelds und Schließen des Textfelds
+    this.setState((prevState) => ({
+      commentForms: {
+        ...prevState.commentForms,
+        [index]: ''
+      }
+    }));
+  };
   render() {
+    const { commentForms } = this.state;
     return (
       <div className="con">
         {/*<div className='header'>
@@ -20,7 +60,18 @@ class News extends Component {
                 <h3 className='news-content'>Unter dem Motto „Beer & Dine“ haben der Deutsche Brauer-Bund erstmals gemeinsam zu einem Abendempfang eingeladen.</h3>
               </a>
               <p className='news-meta'>Veröffentlicht: 10.04.2023</p>
-              <Button variant="primary">Comment</Button>{' '}
+              {commentForms[0] ? (
+                <form onSubmit={(event) => this.handleSubmitComment(event, 0)}>
+                  <textarea
+                    value={commentForms[0]}
+                    onChange={(event) => this.handleCommentChange(event, 0)}
+                    placeholder="Kommentar eingeben"
+                  />
+                  <Button variant="primary" type="submit">Absenden</Button>
+                </form>
+              ) : (
+                <Button variant="primary" onClick={() => this.handleCommentButtonClick(0)}>Kommentar</Button>
+              )}
               <Button variant="primary">"LikeIcon"</Button>{' '}
             </div>
             <hr className='hr-style' />
@@ -31,7 +82,18 @@ class News extends Component {
                 <h3 className='news-content'>Die Brauerei Waldhaus aus dem Südschwarzwald meldet für 2022 ein Absatzplus von 3,6 Prozent auf 105.000 Hektoliter.</h3>
               </a>
               <p className='news-meta'>Veröffentlicht: 23.03.2023</p>
-              <Button variant="primary">Comment</Button>{' '}
+              {commentForms[1] ? (
+                <form onSubmit={(event) => this.handleSubmitComment(event, 1)}>
+                  <textarea
+                    value={commentForms[1]}
+                    onChange={(event) => this.handleCommentChange(event, 1)}
+                    placeholder="Kommentar eingeben"
+                  />
+                  <Button variant="primary" type="submit">Absenden</Button>
+                </form>
+              ) : (
+                <Button variant="primary" onClick={() => this.handleCommentButtonClick(1)}>Kommentar</Button>
+              )}
               <Button variant="primary">"LikeIcon"</Button>{' '}
             </div>
             <hr className='hr-style' />
@@ -42,7 +104,18 @@ class News extends Component {
                 <h3 className='news-content'>Der noch zu Jahresbeginn als „utopisch“ geltende Bierpreis von 7,50 Euro für den halben Liter in ersten Schankbetrieben sei bereits durchbrochen.</h3>
               </a>
               <p className='news-meta'>Veröffentlicht 20.03.2023</p>
-              <Button variant="primary">Comment</Button>{' '}
+              {commentForms[2] ? (
+                <form onSubmit={(event) => this.handleSubmitComment(event, 2)}>
+                  <textarea
+                    value={commentForms[2]}
+                    onChange={(event) => this.handleCommentChange(event, 2)}
+                    placeholder="Kommentar eingeben"
+                  />
+                  <Button variant="primary" type="submit">Absenden</Button>
+                </form>
+              ) : (
+                <Button variant="primary" onClick={() => this.handleCommentButtonClick(2)}>Kommentar</Button>
+              )}
               <Button variant="primary">"LikeIcon"</Button>{' '}
             </div>
             <hr className='hr-style' />
@@ -53,7 +126,18 @@ class News extends Component {
                 <h3 className='news-content'>Manchmal hat ein Bier zu viel Schaum, manchmal zu wenig. Der Prozess der Schaumentstehung ist überaus komplex und hängt von vielen Parametern ab.</h3>
               </a>
               <p className='news-meta'>Veröffentlicht 01.03.2023</p>
-              <Button variant="primary">Comment</Button>{' '}
+              {commentForms[3] ? (
+                <form onSubmit={(event) => this.handleSubmitComment(event, 3)}>
+                  <textarea
+                    value={commentForms[3]}
+                    onChange={(event) => this.handleCommentChange(event, 3)}
+                    placeholder="Kommentar eingeben"
+                  />
+                  <Button variant="primary" type="submit">Absenden</Button>
+                </form>
+              ) : (
+                <Button variant="primary" onClick={() => this.handleCommentButtonClick(3)}>Kommentar</Button>
+              )}
               <Button variant="primary">"LikeIcon"</Button>{' '}
             </div>
             <hr className='hr-style' />
